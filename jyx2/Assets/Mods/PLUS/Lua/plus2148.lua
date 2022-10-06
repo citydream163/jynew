91f65b2d7105b248f59cb9b68a45f69a5abc7a2b
citydream163 <citydream@163.com>
2022-10-07 00:20:27 +0800

    up

--- jyx2/Assets/Mods/PLUS/Lua/plus2148.lua
+++ jyx2/Assets/Mods/PLUS/Lua/plus2148.lua
@@ -2,7 +2,7 @@ Talk(0, "哎呀不好！");
 DarkScence();
 if (GetCurrentEventID() == 17) then
 	Gotarget = 1;
-else
+elseif (GetCurrentEventID() == 16) then
 	Gotarget = 2;
 end;
 jyx2_MovePlayer(""..Gotarget.."","Level/NavigateObjs");
