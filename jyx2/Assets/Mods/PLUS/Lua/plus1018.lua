e9de8d354a07af4a3fbdee2d857436ac70a4d4d4
citydream163 <citydream@163.com>
2022-10-08 14:49:34 +0800

    Update plus1018.lua

--- jyx2/Assets/Mods/PLUS/Lua/plus1018.lua
+++ jyx2/Assets/Mods/PLUS/Lua/plus1018.lua
@@ -12,6 +12,14 @@ if jyx2_CheckBookAndRepute() == true then goto label1 end;
 ::label1::
 	Talk(0, "真累啊，好想大睡一觉。");
     ModifyEvent(-2, -2, -2, -2, -1, -1, 2199, -1, -1, -1, -2, -2, -2);
-	ModifyEvent(-2, 10, 0, 0, -1,-1,2198, -1, -1, -1, -2, -2, -2);--刷请帖
 	jyx2_WalkFromTo(-1,1);
+	Talk(0, "为了走更远的路，适当的休息也是必须的。我就好好的睡一觉吧！", "", 1);
+    DarkScence();
+    Rest();
+    SetRoleFace(1);
+	jyx2_ReplaceSceneObject("","Dynamic/请帖","1");
+    ModifyEvent(-2, 11, 0, 0, 932, -1, -1, 7968, -1, -1, -1, -1, -1);
+    LightScence();
+    Talk(0, "哎呀，好睡好睡！");
+	ModifyEvent(-2, -2, 0, 0, 931, -1, -1, -1, -1, -1, -2, -2, -2);--刷新床为正常睡觉
 	do return end;
