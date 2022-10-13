28d59b0af126119ba7d57824029e79a3ca955a77
citydream163 <citydream@163.com>
2022-10-13 14:22:21 +0800

    新增：烧秘笈

--- jyx2/Assets/Mods/PLUS/Lua/plus16.lua
+++ jyx2/Assets/Mods/PLUS/Lua/plus16.lua
@@ -1,5 +1,338 @@
-if UseItem(160) == true then goto label0 end;
+if UseItem(160) == true then goto label0
+elseif UseItem(39) == true then goto label39
+elseif UseItem(40) == true then goto label40
+elseif UseItem(42) == true then goto label42
+elseif UseItem(43) == true then goto label43
+elseif UseItem(44) == true then goto label44
+elseif UseItem(45) == true then goto label45
+elseif UseItem(46) == true then goto label46
+elseif UseItem(47) == true then goto label47
+elseif UseItem(48) == true then goto label48
+elseif UseItem(49) == true then goto label49
+elseif UseItem(50) == true then goto label50
+elseif UseItem(51) == true then goto label51
+elseif UseItem(52) == true then goto label52
+elseif UseItem(53) == true then goto label53
+elseif UseItem(54) == true then goto label54
+elseif UseItem(55) == true then goto label55
+elseif UseItem(56) == true then goto label56
+elseif UseItem(57) == true then goto label57
+elseif UseItem(58) == true then goto label58
+elseif UseItem(59) == true then goto label59
+elseif UseItem(60) == true then goto label60
+elseif UseItem(61) == true then goto label61
+elseif UseItem(62) == true then goto label62
+elseif UseItem(63) == true then goto label63
+elseif UseItem(64) == true then goto label64
+elseif UseItem(66) == true then goto label66
+elseif UseItem(67) == true then goto label67
+elseif UseItem(68) == true then goto label68
+elseif UseItem(69) == true then goto label69
+elseif UseItem(70) == true then goto label70
+elseif UseItem(71) == true then goto label71
+elseif UseItem(72) == true then goto label72
+elseif UseItem(73) == true then goto label73
+elseif UseItem(74) == true then goto label74
+elseif UseItem(75) == true then goto label75
+elseif UseItem(76) == true then goto label76
+elseif UseItem(77) == true then goto label77
+elseif UseItem(78) == true then goto label78
+elseif UseItem(79) == true then goto label79
+elseif UseItem(80) == true then goto label80
+elseif UseItem(81) == true then goto label81
+elseif UseItem(82) == true then goto label82
+elseif UseItem(83) == true then goto label83
+elseif UseItem(84) == true then goto label84
+elseif UseItem(85) == true then goto label85
+elseif UseItem(86) == true then goto label86
+elseif UseItem(87) == true then goto label87
+elseif UseItem(88) == true then goto label88
+elseif UseItem(89) == true then goto label89
+elseif UseItem(90) == true then goto label90
+elseif UseItem(91) == true then goto label91
+elseif UseItem(92) == true then goto label92
+elseif UseItem(93) == true then goto label93
+elseif UseItem(94) == true then goto label94
+elseif UseItem(95) == true then goto label95
+else
+	Talk(0, "哇！好痛！这烛火还真烫。");
+end;
     do return end;
 ::label0::
-    Talk(0, "我一定是疯了，才会想把这书烧掉！", "", 1);
+    Talk(0, "我一定是疯了，才会想把这书烧掉！");
+do return end;
+::label39::
+    Talk(0, "<color=orange>"..PaperScroll(39,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(39,-1);
+	AddExp(0,PaperScroll(39,0));
+do return end;
+::label40::
+    Talk(0, "<color=orange>"..PaperScroll(40,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(40,-1);
+	AddExp(0,PaperScroll(40,0));
+do return end;
+::label42::
+    Talk(0, "<color=orange>"..PaperScroll(42,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(42,-1);
+	AddExp(0,PaperScroll(42,0));
+do return end;
+::label43::
+    Talk(0, "<color=orange>"..PaperScroll(43,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(43,-1);
+	AddExp(0,PaperScroll(43,0));
+do return end;
+::label44::
+    Talk(0, "<color=orange>"..PaperScroll(44,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(44,-1);
+	AddExp(0,PaperScroll(44,0));
+do return end;
+::label45::
+    Talk(0, "<color=orange>"..PaperScroll(45,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(45,-1);
+	AddExp(0,PaperScroll(45,0));
+do return end;
+::label46::
+    Talk(0, "<color=orange>"..PaperScroll(46,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(46,-1);
+	AddExp(0,PaperScroll(46,0));
+do return end;
+::label47::
+    Talk(0, "<color=orange>"..PaperScroll(47,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(47,-1);
+	AddExp(0,PaperScroll(47,0));
+do return end;
+::label48::
+    Talk(0, "<color=orange>"..PaperScroll(48,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(48,-1);
+	AddExp(0,PaperScroll(48,0));
+do return end;
+::label49::
+    Talk(0, "<color=orange>"..PaperScroll(49,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(49,-1);
+	AddExp(0,PaperScroll(49,0));
+do return end;
+::label50::
+    Talk(0, "<color=orange>"..PaperScroll(50,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(50,-1);
+	AddExp(0,PaperScroll(50,0));
+do return end;
+::label51::
+    Talk(0, "<color=orange>"..PaperScroll(51,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(51,-1);
+	AddExp(0,PaperScroll(51,0));
+do return end;
+::label52::
+    Talk(0, "<color=orange>"..PaperScroll(52,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(52,-1);
+	AddExp(0,PaperScroll(52,0));
+do return end;
+::label53::
+    Talk(0, "<color=orange>"..PaperScroll(53,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(53,-1);
+	AddExp(0,PaperScroll(53,0));
+do return end;
+::label54::
+    Talk(0, "<color=orange>"..PaperScroll(54,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(54,-1);
+	AddExp(0,PaperScroll(54,0));
+do return end;
+::label55::
+    Talk(0, "<color=orange>"..PaperScroll(55,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(55,-1);
+	AddExp(0,PaperScroll(55,0));
+do return end;
+::label56::
+    Talk(0, "<color=orange>"..PaperScroll(56,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(56,-1);
+	AddExp(0,PaperScroll(56,0));
+do return end;
+::label57::
+    Talk(0, "<color=orange>"..PaperScroll(57,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(57,-1);
+	AddExp(0,PaperScroll(57,0));
+do return end;
+::label58::
+    Talk(0, "<color=orange>"..PaperScroll(58,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(58,-1);
+	AddExp(0,PaperScroll(58,0));
+do return end;
+::label59::
+    Talk(0, "<color=orange>"..PaperScroll(59,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(59,-1);
+	AddExp(0,PaperScroll(59,0));
+do return end;
+::label60::
+    Talk(0, "<color=orange>"..PaperScroll(60,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(60,-1);
+	AddExp(0,PaperScroll(60,0));
+do return end;
+::label61::
+    Talk(0, "<color=orange>"..PaperScroll(61,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(61,-1);
+	AddExp(0,PaperScroll(61,0));
+do return end;
+::label62::
+    Talk(0, "<color=orange>"..PaperScroll(62,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(62,-1);
+	AddExp(0,PaperScroll(62,0));
+do return end;
+::label63::
+    Talk(0, "<color=orange>"..PaperScroll(63,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(63,-1);
+	AddExp(0,PaperScroll(63,0));
+do return end;
+::label64::
+    Talk(0, "<color=orange>"..PaperScroll(64,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(64,-1);
+	AddExp(0,PaperScroll(64,0));
+do return end;
+::label66::
+    Talk(0, "<color=orange>"..PaperScroll(66,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(66,-1);
+	AddExp(0,PaperScroll(66,0));
+do return end;
+::label67::
+    Talk(0, "<color=orange>"..PaperScroll(67,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(67,-1);
+	AddExp(0,PaperScroll(67,0));
+do return end;
+::label68::
+    Talk(0, "<color=orange>"..PaperScroll(68,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(68,-1);
+	AddExp(0,PaperScroll(68,0));
+do return end;
+::label69::
+    Talk(0, "<color=orange>"..PaperScroll(69,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(69,-1);
+	AddExp(0,PaperScroll(69,0));
+do return end;
+::label70::
+    Talk(0, "<color=orange>"..PaperScroll(70,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(70,-1);
+	AddExp(0,PaperScroll(70,0));
+do return end;
+::label71::
+    Talk(0, "<color=orange>"..PaperScroll(71,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(71,-1);
+	AddExp(0,PaperScroll(71,0));
+do return end;
+::label72::
+    Talk(0, "<color=orange>"..PaperScroll(72,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(72,-1);
+	AddExp(0,PaperScroll(72,0));
+do return end;
+::label73::
+    Talk(0, "<color=orange>"..PaperScroll(73,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(73,-1);
+	AddExp(0,PaperScroll(73,0));
+do return end;
+::label74::
+    Talk(0, "<color=orange>"..PaperScroll(74,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(74,-1);
+	AddExp(0,PaperScroll(74,0));
+do return end;
+::label75::
+    Talk(0, "<color=orange>"..PaperScroll(75,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(75,-1);
+	AddExp(0,PaperScroll(75,0));
+do return end;
+::label76::
+    Talk(0, "<color=orange>"..PaperScroll(76,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(76,-1);
+	AddExp(0,PaperScroll(76,0));
+do return end;
+::label77::
+    Talk(0, "<color=orange>"..PaperScroll(77,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(77,-1);
+	AddExp(0,PaperScroll(77,0));
+do return end;
+::label78::
+    Talk(0, "<color=orange>"..PaperScroll(78,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(78,-1);
+	AddExp(0,PaperScroll(78,0));
+do return end;
+::label79::
+    Talk(0, "<color=orange>"..PaperScroll(79,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(79,-1);
+	AddExp(0,PaperScroll(79,0));
+do return end;
+::label80::
+    Talk(0, "<color=orange>"..PaperScroll(80,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(80,-1);
+	AddExp(0,PaperScroll(80,0));
+do return end;
+::label81::
+    Talk(0, "<color=orange>"..PaperScroll(81,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(81,-1);
+	AddExp(0,PaperScroll(81,0));
+do return end;
+::label82::
+    Talk(0, "<color=orange>"..PaperScroll(82,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(82,-1);
+	AddExp(0,PaperScroll(82,0));
+do return end;
+::label83::
+    Talk(0, "<color=orange>"..PaperScroll(83,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(83,-1);
+	AddExp(0,PaperScroll(83,0));
+do return end;
+::label84::
+    Talk(0, "<color=orange>"..PaperScroll(84,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(84,-1);
+	AddExp(0,PaperScroll(84,0));
+do return end;
+::label85::
+    Talk(0, "<color=orange>"..PaperScroll(85,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(85,-1);
+	AddExp(0,PaperScroll(85,0));
+do return end;
+::label86::
+    Talk(0, "<color=orange>"..PaperScroll(86,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(86,-1);
+	AddExp(0,PaperScroll(86,0));
+do return end;
+::label87::
+    Talk(0, "<color=orange>"..PaperScroll(87,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(87,-1);
+	AddExp(0,PaperScroll(87,0));
+do return end;
+::label88::
+    Talk(0, "<color=orange>"..PaperScroll(88,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(88,-1);
+	AddExp(0,PaperScroll(88,0));
+do return end;
+::label89::
+    Talk(0, "<color=orange>"..PaperScroll(89,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(89,-1);
+	AddExp(0,PaperScroll(89,0));
+do return end;
+::label90::
+    Talk(0, "<color=orange>"..PaperScroll(90,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(90,-1);
+	AddExp(0,PaperScroll(90,0));
+do return end;
+::label91::
+    Talk(0, "<color=orange>"..PaperScroll(91,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(91,-1);
+	AddExp(0,PaperScroll(91,0));
+do return end;
+::label92::
+    Talk(0, "<color=orange>"..PaperScroll(92,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(92,-1);
+	AddExp(0,PaperScroll(92,0));
+do return end;
+::label93::
+    Talk(0, "<color=orange>"..PaperScroll(93,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(93,-1);
+	AddExp(0,PaperScroll(93,0));
+do return end;
+::label94::
+    Talk(0, "<color=orange>"..PaperScroll(94,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(94,-1);
+	AddExp(0,PaperScroll(94,0));
+do return end;
+::label95::
+    Talk(0, "<color=orange>"..PaperScroll(95,1).."</color>留着也没用了，一把火烧了才干净！");
+	AddItem(95,-1);
+	AddExp(0,PaperScroll(95,0));
 do return end;
