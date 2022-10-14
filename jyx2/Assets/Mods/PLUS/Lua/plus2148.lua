e0360f2fa5bfea81f5cfca8e3e3bcdb3667a90d1
citydream163 <citydream@163.com>
2022-10-14 11:42:08 +0800

    更新桃花岛地图

--- jyx2/Assets/Mods/PLUS/Lua/plus2148.lua
+++ jyx2/Assets/Mods/PLUS/Lua/plus2148.lua
@@ -4,6 +4,14 @@ if (GetCurrentEventID() == 17) then
 	Gotarget = 1;
 elseif (GetCurrentEventID() == 16) then
 	Gotarget = 2;
+elseif (GetCurrentEventID() == 20) then
+	Gotarget = 0;
+elseif (GetCurrentEventID() == 21) then
+	Gotarget = 0;
+elseif (GetCurrentEventID() == 22) then
+	Gotarget = 0;
+elseif (GetCurrentEventID() == 23) then
+	Gotarget = 0;
 end;
 jyx2_MovePlayer(""..Gotarget.."","Level/NavigateObjs");
 LightScence();
